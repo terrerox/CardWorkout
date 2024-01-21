e98a8289ff0acd093029d453c3c8bf6322cb9fa8
terrerox <terrero0611@gmail.com>
2024-01-21 16:43:54 -0400

    init

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,46 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by terrerox on 25/12/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+    
+    @IBOutlet var cardImageView: UIImageView!
+    
+    var cards: [UIImage] = Deck.allValues
+    
+    var timer: Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
+    }
+}
